a2c6f7686733862329ee042562cd9088679244d7
Ninciolo <ingegneriadelmovimento@gmail.com>
2025-09-03 21:21:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,41 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "creatore-loggia-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+// Installazione: salva nella cache
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('hex-app-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+// Attivazione: pulizia vecchia cache
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch: risponde prima con la cache, poi con la rete
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });

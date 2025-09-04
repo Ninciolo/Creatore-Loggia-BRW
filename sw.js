7c880a8b8220ac00d9399df1c9c05ea7050e6e98
Ninciolo <ingegneriadelmovimento@gmail.com>
2025-09-04 15:24:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open("loggia-cache").then(cache => cache.addAll([
-    "./",
-    "index.html",
-    "manifest.json"
-  ])));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('loggia-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icons/icon-192.png',
+        './icons/icon-512.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
